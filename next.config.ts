e7e79ad4e7aaa9d8c857af776bc2a2238d8de251
Ferran Buireu <fbuireu@gmail.com>
2025-07-25 22:20:10 +0200

    ci: fix bindings

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,21 @@
-import type { NextConfig } from "next";
-import createNextIntlPlugin from "next-intl/plugin";
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+import type { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin("./src/infrastructure/i18n/config/config.ts");
+const withNextIntl = createNextIntlPlugin('./src/infrastructure/i18n/config/config.ts');
 
 const nextConfig: NextConfig = {
-	trailingSlash: false,
-	experimental: {
-		cssChunking: true,
-		inlineCss: true,
-		viewTransition: true,
-		webVitalsAttribution: ["FCP", "CLS", "TTFB", "FID", "LCP", "INP"],
-	},
+  trailingSlash: false,
+  experimental: {
+    cssChunking: true,
+    inlineCss: true,
+    viewTransition: true,
+    webVitalsAttribution: ['FCP', 'CLS', 'TTFB', 'FID', 'LCP', 'INP'],
+  },
 };
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default withNextIntl(nextConfig);
