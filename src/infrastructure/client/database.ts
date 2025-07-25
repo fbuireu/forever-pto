484061d006448de6b49ab3dd4699b7d703335eb5
Ferran Buireu <fbuireu@gmail.com>
2025-07-25 20:54:06 +0200

    fix: restore DB init buildtime

--- src/infrastructure/client/database.ts
+++ src/infrastructure/client/database.ts
@@ -1,25 +1,6 @@
-import { type Client, createClient } from "@libsql/client";
+import { createClient } from "@libsql/client";
 
-let database: Client | undefined;
-
-export function getDatabase() {
-	if (database) return database;
-
-	const url = process.env.TURSO_DATABASE_URL;
-	const authToken = process.env.TURSO_AUTH_TOKEN;
-
-	if (!url) {
-		throw new Error("Missing TURSO_DATABASE_URL");
-	}
-
-	if (!authToken) {
-		throw new Error("Missing TURSO_AUTH_TOKEN");
-	}
-
-	database = createClient({
-		url,
-		authToken,
-	});
-
-	return database;
-}
+export const database = createClient({
+	url: process.env.TURSO_DATABASE_URL ?? "",
+	authToken: process.env.TURSO_AUTH_TOKEN,
+});
