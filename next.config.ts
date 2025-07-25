eb3f45aea4c7cb12388b15f6bcdef07770fcd8b2
Ferran Buireu <fbuireu@gmail.com>
2025-07-25 21:13:12 +0200

    ci: fix output

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,17 @@
-import type { NextConfig } from "next";
-import createNextIntlPlugin from "next-intl/plugin";
+import type { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin("./src/infrastructure/i18n/config/config.ts");
+const withNextIntl = createNextIntlPlugin('./src/infrastructure/i18n/config/config.ts');
 
 const nextConfig: NextConfig = {
-	experimental: {
-		cssChunking: true,
-		inlineCss: true,
-		viewTransition: true,
-		webVitalsAttribution: ["FCP", "CLS", "TTFB", "FID", "LCP", "INP"],
-	},
-	trailingSlash: false,
-	env: {},
+  output: 'export',
+  trailingSlash: false,
+  experimental: {
+    cssChunking: true,
+    inlineCss: true,
+    viewTransition: true,
+    webVitalsAttribution: ['FCP', 'CLS', 'TTFB', 'FID', 'LCP', 'INP'],
+  },
 };
 
 export default withNextIntl(nextConfig);
