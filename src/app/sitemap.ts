ca303bb6910ce54d6614859e79c82e645af2c45c
Ferran Buireu <fbuireu@gmail.com>
2025-07-26 12:53:07 +0200

    feat: add sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,15 +4,15 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
 	const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? "";
 
-	return [
-		{
-			url: baseUrl,
-			lastModified: new Date(),
-			changeFrequency: "weekly",
-			priority: 1,
-			alternates: {
-				languages: Object.fromEntries(I18N_CONFIG.LOCALES.map((lang) => [lang, `${baseUrl}/${lang}`])),
-			},
+	const sitemapEntries: MetadataRoute.Sitemap = I18N_CONFIG.LOCALES.map((locale) => ({
+		url: `${baseUrl}/${locale}`,
+		lastModified: new Date(),
+		changeFrequency: "weekly" as const,
+		priority: locale === I18N_CONFIG.DEFAULT_LOCALE ? 1 : 0.8,
+		alternates: {
+			languages: Object.fromEntries(I18N_CONFIG.LOCALES.map((lang) => [lang, `${baseUrl}/${lang}`])),
 		},
-	];
+	}));
+
+	return sitemapEntries;
 }
