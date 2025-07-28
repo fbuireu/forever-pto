c10b00f8f79a1afb6d237b17bf396b8623cb0eb3
Ferran <fbuireu@gmail.com>
2025-07-28 13:25:14 +0200

    feat: add sliding number

--- src/ui/modules/components/core/Header.tsx
+++ src/ui/modules/components/core/Header.tsx
@@ -1,25 +1,18 @@
 'use client'
 
 import { usePtoDays, useSetPtoDays } from "@application/stores/pto";
+import { Counter } from "src/components/animate-ui/components/counter";
 
 export const Header = () => {
   const ptoDays = usePtoDays();
   const setPtoDays = useSetPtoDays(); 
   
-  const incrementPtoDays = () => {
-    setPtoDays(ptoDays + 1);
-  };
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <div className="text-center">
         <div className="mt-4">
           <p className="text-xl mb-2">PTO Days: {ptoDays}</p>
-          <button 
-            onClick={incrementPtoDays}
-            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-          >
-            +1 PTO Day
-          </button>
+          <Counter number={ptoDays} setNumber={setPtoDays} />
         </div>
       </div>
     </div>
