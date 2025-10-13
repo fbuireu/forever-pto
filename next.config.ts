10832e0bc7806e7cab104c4701d584bfaf81ed35
Ferran <fbuireu@gmail.com>
2025-10-13 18:23:50 +0200

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,16 @@
-//import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
-import type { NextConfig } from "next";
-
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-	trailingSlash: false,
-	experimental: {
-		cssChunking: true,
-		inlineCss: true,
-		viewTransition: true,
-		webVitalsAttribution: ["FCP", "CLS", "TTFB", "FID", "LCP", "INP"],
-	},
+  trailingSlash: false,
+  experimental: {
+    cssChunking: true,
+    inlineCss: true,
+    viewTransition: true,
+    webVitalsAttribution: ['FCP', 'CLS', 'TTFB', 'FID', 'LCP', 'INP'],
+  },
 };
 
-if (process.env.NODE_ENV === "development") {
-	// await setupDevPlatform();
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
 }
-
