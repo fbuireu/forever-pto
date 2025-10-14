cc820d733e04fd6785ce1fedbaf1aa9e0e9edbae
Ferran <fbuireu@gmail.com>
2025-10-14 09:05:01 +0200

    ci: add incremental cache support

--- environment.d.ts
+++ environment.d.ts
@@ -14,7 +14,6 @@ declare global {
       JWT_SECRET: string;
       NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: string;
       STRIPE_SECRET_KEY: string;
-      JWT_SECRET: string;
       STRIPE_WEBHOOK_SECRET: string;
       RESEND_API_KEY: string;
     }
