dce5d53c91337ae3914bca8d55ebc9fa7f0b9461
Ferran <fbuireu@gmail.com>
2025-10-30 09:00:22 +0100

    chore: bump deps

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
-import { withBetterStack } from '@logtail/next';
-import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
-import type { NextConfig } from 'next';
-import createNextIntlPlugin from 'next-intl/plugin';
+import { withBetterStack } from '@logtail/next'
+import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare'
+import type { NextConfig } from 'next'
+import createNextIntlPlugin from 'next-intl/plugin'
 
-const withNextIntl = createNextIntlPlugin('./src/infrastructure/i18n/config.ts');
+const withNextIntl = createNextIntlPlugin('./src/infrastructure/i18n/config.ts')
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig: NextConfig = {
+  cacheComponents: true,
   compiler: {
     removeConsole: isProd,
   },
@@ -20,8 +21,11 @@ const nextConfig: NextConfig = {
     '@libsql/isomorphic-ws',
     '@libsql/isomorphic-fetch',
   ],
-};
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  }
+}
 
-export default withBetterStack(withNextIntl(nextConfig));
+export default withBetterStack(withNextIntl(nextConfig))
 
-initOpenNextCloudflareForDev();
+initOpenNextCloudflareForDev()
