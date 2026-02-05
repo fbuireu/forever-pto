74e169d60dabcff8a9c86a0329aa8429c615e3c7
Ferran <Ferran.Buireu+gartner@gartner.com>
2026-02-05 09:04:56 +0100

    fix: i18n config

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,15 @@ import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
 import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin('./src/infrastructure/i18n/config.ts', {
-  messages: {
-    path: './src/ui/i18n/messages',
-    locales: 'infer',
-    format: 'json',
-    precompile: true,
+const withNextIntl = createNextIntlPlugin({
+  requestConfig: './src/infrastructure/i18n/config.ts',
+  experimental: {
+    messages: {
+      path: './src/ui/i18n/messages',
+      locales: 'infer',
+      format: 'json',
+      precompile: true,
+    },
   },
 });
 
