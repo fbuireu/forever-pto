6a9e3847812c9e2f67095a1e3254690e997139d1
Ferran Buireu <fbuireu@gmail.com>
2026-04-18 00:07:30 +0200

    test ci (#276)
    * ci: test dev branch deployment
    
    * ci: test dev branch deployment
    
    * ci: test dev branch deployment
    
    * ci: test dev branch deployment
    
    * fix: ci sample log
    
    * chore(release): 1.0.1 [skip ci]
    
    ## [1.0.1](https://github.com/fbuireu/forever-pto/compare/v1.0.0...v1.0.1) (2026-04-17)
    
    ### Bug Fixes
    
    * ci sample log ([c5fe2e4](https://github.com/fbuireu/forever-pto/commit/c5fe2e4cb5392c7b12517cc7a81e6b099128d0b4))
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * ci: add e2e
    
    * fix: ci
    
    * fix: variables ci
    
    ---------
    
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,15 +1,6 @@
 import { LOCALES } from '@infrastructure/i18n/config';
 import { Faq } from '@ui/modules/components/home/Faq';
 import { JsonLd } from '@ui/modules/components/seo/JsonLd';
-<<<<<<< Updated upstream
-import type { Locale } from 'next-intl';
-import { setRequestLocale } from 'next-intl/server';
-import dynamic from 'next/dynamic';
-
-const HolidaysList = dynamic(() => import('@ui/modules/components/home/HolidaysList').then((mod) => mod.HolidaysList));
-const ManagementBar = dynamic(() =>
-  import('@ui/modules/components/home/ManagementBar').then((mod) => mod.ManagementBar),
-=======
 import dynamic from 'next/dynamic';
 import type { Locale } from 'next-intl';
 import { setRequestLocale } from 'next-intl/server';
@@ -17,7 +8,6 @@ import { setRequestLocale } from 'next-intl/server';
 const HolidaysList = dynamic(() => import('@ui/modules/components/home/HolidaysList').then((mod) => mod.HolidaysList));
 const ManagementBar = dynamic(() =>
   import('@ui/modules/components/home/ManagementBar').then((mod) => mod.ManagementBar)
->>>>>>> Stashed changes
 );
 const CalendarList = dynamic(() => import('@ui/modules/components/home/CalendarList').then((mod) => mod.CalendarList));
 const Legend = dynamic(() => import('@ui/modules/components/home/Legend').then((mod) => mod.Legend));
@@ -58,11 +48,7 @@ const Home = async ({ params }: LayoutProps) => {
 
 export default Home;
 
-<<<<<<< Updated upstream
 // todo: add funding
-=======
-// todo: add funding 2
->>>>>>> Stashed changes
 // todo: fix auth in Renovate PRs in CF
 // todo: fix "we use cookies"
 // todo: bug apply alternatives
