93fff50ca95bff05dd31d1f24d8490a5abe9dc5a
Ferran Buireu <fbuireu@gmail.com>
2026-04-26 09:35:42 +0200

    style: correct tab

--- src/ui/modules/pages/homepage/navigation/HomepageLanguageSwitcher.tsx
+++ src/ui/modules/pages/homepage/navigation/HomepageLanguageSwitcher.tsx
@@ -2,6 +2,7 @@
 
 import { usePathname, useRouter } from '@application/i18n/navigtion';
 import { usePremiumStore } from '@application/stores/premium';
+import { useLanguages } from '@ui/hooks/useLanguages';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -11,11 +12,10 @@ import {
 import { Button } from '@ui/modules/core/animate/components/buttons/Button';
 import { Check } from '@ui/modules/core/animate/icons/Check';
 import { AnimateIcon } from '@ui/modules/core/animate/icons/Icon';
-import { useLanguages } from '@ui/hooks/useLanguages';
 import { useLocale, useTranslations } from 'next-intl';
 import { useCallback, useEffect, useMemo } from 'react';
 
-export const HomepageLanguageSwitcher    = () => {
+export const HomepageLanguageSwitcher = () => {
   const locale = useLocale();
   const router = useRouter();
   const pathname = usePathname();
