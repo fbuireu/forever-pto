f87176b477b7466cd6431dd511ce69eae146c002
Ferran Buireu <fbuireu@gmail.com>
2026-05-10 11:00:41 +0200

    ci: add codecov

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     exclude: ['e2e/**', 'node_modules/**'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+    },
   },
 });
