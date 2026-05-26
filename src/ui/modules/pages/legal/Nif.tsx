a8eba588ce42da0fe903b0cda97e1cbd5a9cf24b
Ferran Buireu <fbuireu@gmail.com>
2026-05-26 07:22:48 +0200

    chore: bump deps

---
+++ src/ui/modules/pages/legal/Nif.tsx
@@ -0,0 +1,27 @@
+const CHARS = [
+  { character: '2', order: 4 },
+  { character: '4', order: 0 },
+  { character: '7', order: 7 },
+  { character: '9', order: 2 },
+  { character: '9', order: 6 },
+  { character: '6', order: 1 },
+  { character: 'H', order: 8 },
+  { character: '7', order: 3 },
+  { character: '2', order: 5 },
+];
+
+interface NifProps {
+  ariaLabel: string;
+}
+
+export const Nif = ({ ariaLabel }: NifProps) => {
+  return (
+    <span className='inline-flex select-none' role='img' aria-label={ariaLabel}>
+      {CHARS.map(({ character, order }) => (
+        <span key={order} style={{ order }}>
+          {character}
+        </span>
+      ))}
+    </span>
+  );
+};
