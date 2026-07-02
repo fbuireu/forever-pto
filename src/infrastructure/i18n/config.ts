7a49b0969a02ce01f4d103edfc42328d6a2a2f4d
Ferran Buireu <fbuireu@gmail.com>
2026-07-02 12:20:13 +0000

    style: sort i18n config imports

--- src/infrastructure/i18n/config.ts
+++ src/infrastructure/i18n/config.ts
@@ -1,10 +1,10 @@
-import { routing } from '@infrastructure/i18n/routing';
 import ca from '@i18n/messages/ca.json';
 import de from '@i18n/messages/de.json';
 import en from '@i18n/messages/en.json';
 import es from '@i18n/messages/es.json';
 import fr from '@i18n/messages/fr.json';
 import it from '@i18n/messages/it.json';
+import { routing } from '@infrastructure/i18n/routing';
 import { type AbstractIntlMessages, hasLocale } from 'next-intl';
 import { getRequestConfig } from 'next-intl/server';
 import { LOCALES } from './locales';
